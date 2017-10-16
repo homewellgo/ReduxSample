4ac9b7d6f863117f32f8d24f2eccfa4a79ad359c
HomeWellGo <homewellgo_homewellgo190@gmail.com>
2017-10-16 23:14:21 +0900

    create SignUpViewController and add UI

---
+++ ReduxSample/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  ReduxSample
+//
+//  Created by PB2 on 2017/10/16.
+//  Copyright © 2017年 homewellgo. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
